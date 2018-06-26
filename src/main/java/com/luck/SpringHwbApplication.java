596375cfb9d1b983cf2706a54e87eb40f714e595
hwb <luckhwb@163.com>
2018-06-26 10:10:37 +0800

    update package

--- src/main/java/com/luck/SpringHwbApplication.java
+++ src/main/java/com/luck/SpringHwbApplication.java
@@ -2,11 +2,18 @@ package com.luck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringHwbApplication {
+public class SpringHwbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHwbApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringHwbApplication.class);
 	}
 }
